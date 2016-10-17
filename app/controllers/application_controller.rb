a695776d1d3dae01144e6cc61b4c980ebf9049bd
ztx1218 <ztx1218@gmail.com>
2016-10-17 21:53:32 +0800

    test'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,20 @@ class ApplicationController < ActionController::Base
   def hello
       render html: "hello world!"
   end
+ def index
+    # Choose the URL to visit
+    @app_url = "http://www.marinetraffic.com/en/ais/index/ships/all/_:c35428865d60c383702431a7d936ca65/flag:CN/shipname:MIL" 
+    # Ensure it starts with http
+    @app_url = "http://#{@app_url}" unless @app_url.starts_with?("http")
+
+    begin
+      # Retrieve the webpage
+      @news = HTTParty.get(@app_url)
+    rescue StandardError 
+      # When something goes wrong create a fallback message
+      @news = OpenStruct.new(:code => nil, :message => "Domain not found")
+    end
+    
+    render html: "Test" 
+  end
 end
